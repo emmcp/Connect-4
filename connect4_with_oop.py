b9576327cf1fcfe42800cb195d940f38ee5be0e6
emmcp <emma-mcpearce@uiowa.edu>
2024-12-08 20:18:26 -0600

    connect4_with_oop.py
    Adding player info and starting saving

--- connect4_with_oop.py
+++ connect4_with_oop.py
@@ -1,10 +1,12 @@
 from tkinter import *
+import random
+import sqlite3
 
 global root # allows root to be accessed through all classes
 root = Tk()
 root.title("Connect4")
 
-class Players:
+class Players: # something here is buggy so not calling
     def __init__(self):
         self.player1 = {"Name": "Player 1", "Turns Taken": 0, "Pieces": 21} # can use dictionaries to count turns and number of pieces gone
         self.player2 = {"Name": "Player 2", "Turns Taken": 0, "Pieces": 21}
@@ -47,7 +49,6 @@ class Board: # setting up the board, tracking game
             elif self.player1["Color"] == "Yellow":
                 piece = "Yellow"
         
-
 class GUI: # interface elements
     def __init__(self):
         self.root = root
@@ -61,9 +62,9 @@ class GUI: # interface elements
 
     def button_initalizing(self): # making the buttons
         # lambda allows one to be added to each corresponding column of self.pieces
-        self.buttonA = Button(self.root, text="Column A", command = lambda: self.clicked(0), bg = "Navy")
+        self.buttonA = Button(root, text="Column A", command = lambda: self.clicked(0), bg = "Navy")
         self.buttonA.grid(row=2, column=1)
-        self.buttonB = Button(self.root, text="Column B", command = lambda: self.clicked(1), bg = "Navy")
+        self.buttonB = Button(root, text="Column B", command = lambda: self.clicked(1), bg = "Navy")
         self.buttonB.grid(row=2, column=2)
         self.buttonC = Button(root, text="Column C", command = lambda: self.clicked(2), bg = "Navy")
         self.buttonC.grid(row=2, column=3)
@@ -84,7 +85,8 @@ class GUI: # interface elements
 
 class GameInstructions:
     def __init__(self):
-        self.instructions = instructions
+        self.instructions = ("Take turns dropping one of your pieces onto the board.", 
+        "The first player to get four pieces in a row, vertically, horizontally, or diagnoally, wins!")
 
     def game_instructions(self):
         instructions = ("Take turns dropping one of your pieces onto the board.", 
@@ -93,16 +95,23 @@ class GameInstructions:
     def display_instructions():
         messagebox.showinfo("Connect 4 Instructions", instructions)
 
-# class DataSave:
-#     def __init__(self, database_routing):
-#         self.game_save = game_save
+# class GameSave:
+#     def __init__(self):
 #         self.con = sqlite3.connect('game_progress.db')
-#         self.cur = con.cursor()
-#     # to be continued once rest is done; need save variables before
-# #    cur.execute('''CREATE TABLE IF NOT EXISTS gameprogress
-# #                (Player 1 Score, Player 2 Score, Position)''')
+#         self.cur = self.con.cursor()
+#         self.retrieve_data()
+
+#     def retrieve_data(self):
+#         self.cur.execute('''CREATE TABLE IF NOT EXISTS gameprogress
+#                 (Player_1_Score BIT, Player_2_Score BIT)''')
+#         self.cur.execute('''INSERT INTO gameprogress VALUES 
+#                 (self.player1[Score], self.player2["Score"])''')
+#         self.con.commit()
 
 # calling functions
+#p = Players()
 b = Board()
 g = GUI()
+i = GameInstructions()
+#data = GameSave()
 root.mainloop()
